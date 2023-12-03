0d270a3a19c8d2a3ab21f1d93cbc560849d6a3de
Md. Akbar Hossain <smakbor33@gmail.com>
2023-12-03 20:15:36 +0600

    finance models

--- sms/finance/models.py
+++ sms/finance/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from students.models import student
 
 # Create your models here.
+class student_ac(models.Model):
+    balance=models.FloatField()
+    ac_no=models.ForeignKey(student, on_delete=models.CASCADE, unique=True, primary_key=True)
+
+class transection_detail(models.Model):
+    trxdid=models.AutoField(primary_key=True)
+    admition_fee=models.FloatField()
+    tution_fee=models.FloatField()
+    exam_fee=models.FloatField()
+    computer_lab=models.FloatField()
+    student_activity_fee=models.FloatField()
+    library_fee=models.FloatField()
+    scout_fee=models.FloatField()
+    development_charge=models.FloatField()
+    id_card=models.FloatField()
+    fine=models.FloatField()
+
+class transection(models.Model):
+    trxid=models.CharField(max_length=25, unique=True)
+    amount=models.FloatField()
+    get_way=models.CharField(max_length=30)
+    created_at=models.DateTimeField(auto_now_add=True)
+    ac_no=models.ForeignKey(student_ac,on_delete=models.CASCADE)
+    trxdid=models.ForeignKey(transection_detail, on_delete=models.CASCADE)
+
